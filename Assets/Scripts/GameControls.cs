2615eb7dac65bfd7181026cc613586b9ac3d84f7
WarpSonic <CaiusJH@outlook.com>
2022-07-20 15:48:31 -0500

    DontTouchTheCubes B1

---
+++ Assets/Scripts/GameControls.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameControls: MonoBehaviour
+{
+    private Text timerText;
+    private int timerCount;
+
+    void Start()
+    {
+        Time.timeScale = 1f;
+
+        StartCoroutine(CountTime());
+
+        timerText = GameObject.Find("Score").GetComponent<Text>();
+    }
+    IEnumerator CountTime()
+    {
+        yield return new WaitForSeconds(1f);
+        timerCount++;
+        timerText.text = "Score: " + timerCount;
+        StartCoroutine(CountTime());
+    }
+}
